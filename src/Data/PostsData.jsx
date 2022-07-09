5d8f9394eea042ffaa813e7c136706acb04cbef5
madscorpio20 <madhavkumarrai@gmail.com>
2022-07-09 20:21:35 +0530

    posts display section

---
+++ src/Data/PostsData.jsx
@@ -0,0 +1,28 @@
+import postpic1 from "../img/postpic1.jpg";
+import postpic2 from "../img/postpic2.jpg";
+import postpic3 from "../img/postpic3.JPG";
+
+export const PostsData = [
+    {
+        img: postpic1,
+        name: 'Tzuyu',
+        desc: 'Happy new yeard to all! #2022',
+        likes: 2032,
+        liked: true
+    },
+    {
+        img: postpic2,
+        name: 'Maryam',
+        desc: 'Party time :)',
+        likes: 102,
+        liked: false
+    },
+    {
+        img: postpic3,
+        name: 'Salena Gomez',
+        desc: 'At archery fastival',
+        likes: 73,
+        liked: true
+    }
+
+]
