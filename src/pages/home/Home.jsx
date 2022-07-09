5827b1cc96e7cc8d40566905264ccd7ddd1db246
madscorpio20 <madhavkumarrai@gmail.com>
2022-07-09 18:01:22 +0530

    post share

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Home.css";
 import ProfileSide from "../../components/profileSide/profileSide";
+import PostSide from "../../components/PostSide/PostSide";
 const Home = ()=>{
     return(
         <div className="home">
             <ProfileSide />
-            <div className="postSide">Posts</div>
+            <PostSide />
             <div className="rightSide">Right side</div>
         </div>
     )
