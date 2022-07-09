d5900bff8c3d18b09d3c40d731fc55f591852cd1
madscorpio20 <madhavkumarrai@gmail.com>
2022-07-09 20:44:05 +0530

    right side nav bar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./Home.css";
 import ProfileSide from "../../components/profileSide/profileSide";
 import PostSide from "../../components/PostSide/PostSide";
+import RightSide from "../../components/RightSide/RightSide";
 const Home = ()=>{
     return(
         <div className="home">
             <ProfileSide />
             <PostSide />
-            <div className="rightSide">Right side</div>
+            <RightSide />
         </div>
     )
 }
