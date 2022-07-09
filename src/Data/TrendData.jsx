83d04a834d540c53d915bfe363491c4ccc039ac7
madscorpio20 <madhavkumarrai@gmail.com>
2022-07-09 21:33:42 +0530

    Trends Card

---
+++ src/Data/TrendData.jsx
@@ -0,0 +1,23 @@
+export const TrendData = [
+    {
+        name: 'Minions',
+        share: 97
+    },
+    {
+        name: 'Avangers',
+        share: 80.6
+    },
+    {
+        name: 'Elon Musk',
+        share: 72
+    },
+    {
+        name: 'Meta',
+        share: 32
+    },
+    {
+        name: 'MERN',
+        share: 12.2
+    }
+
+]
--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -3,10 +3,11 @@ import "./RightSide.css";
 import Home from "../../img/home.png";
 import Noti from "../../img/noti.png";
 import Comment from "../../img/comment.png";
-import {UilSetting } from '@iconscout/react-unicons';
+import { UilSetting } from '@iconscout/react-unicons';
+import TrendCard from "../TrendCard/TrendCard";
 
-const RightSide = ()=>{
-    return(
+const RightSide = () => {
+    return (
         <div className="RightSide">
             <div className="navIcons">
                 <img src={Home} alt="" />
@@ -14,6 +15,10 @@ const RightSide = ()=>{
                 <img src={Noti} alt="" />
                 <img src={Comment} alt="" />
             </div>
+            <TrendCard />
+            <button className="button r-button">
+                Share
+            </button>
         </div>
     );
 }
